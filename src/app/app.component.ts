455e2848cc1ffa824a36f83dda5b692e27496106
Mauro Alves Aguiar <mauro.alves.aguiar@outlook.com>
2024-03-22 12:39:53 -0300

    Inclusão de arquivos iniciais

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/header/header.component";
+import { FooterComponent } from "./shared/footer/footer.component";
+
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HeaderComponent, FooterComponent]
+})
+export class AppComponent {
+  title = 'crud-angular';
+}
